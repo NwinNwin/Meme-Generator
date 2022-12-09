e5ad80697cb48c2b5e63dc32ee619a349f9ab9a3
NwinNwin <ndnguyen2003@gmail.com>
2022-12-09 11:55:32 -0800

    generate random meme images from static data

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage:"http://i.imgflip.com/1bij.jpg",
+    })
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+    function getMemes(){
+        let memesArray = allMemeImages.data.memes;
+        let newMeme = memesArray[Math.floor(Math.random()*memesArray.length)].url;
+        setMeme(prev => ({
+         ...prev,
+         randomImage:newMeme,
+        }))
+    }
+
     return(
-        <form action="" className="meme-form">
-            <div className="meme-input">
-                <input type="text" placeholder="Shut up" className="meme-input-box"/>
-                <input type="text" placeholder="and take my money" className="meme-input-box"/>
+        <div className="meme-page">
+            <div className="meme-form">
+            
+                <div className="meme-input">
+                    <input type="text" placeholder="Shut up" className="meme-input-box"/>
+                    <input type="text" placeholder="and take my money" className="meme-input-box"/>
+                </div>
+                <button onClick={getMemes} className="meme-button"><h2>Get a new meme image 🖼️</h2></button>
+                
             </div>
-            <button className="meme-button"><h2>Get a new meme image 🖼️</h2></button>
-        </form>
+            <img className="meme-img" src={meme.randomImage} alt="" />
+        </div>
     )
 }
