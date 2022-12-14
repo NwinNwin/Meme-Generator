1c74a492aacbe996733f1ff7b57bfb67ac6e89ec
NwinNwin <ndnguyen2003@gmail.com>
2022-12-13 19:41:18 -0800

    add the ability to add texts to the meme img

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -18,19 +18,31 @@ export default function Meme() {
          randomImage:newMeme,
         }))
     }
+    
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prev => {return {
+            ...prev,
+            [name] : value
+        }})
+    }
 
     return(
         <div className="meme-page">
             <div className="meme-form">
             
                 <div className="meme-input">
-                    <input type="text" placeholder="Shut up" className="meme-input-box"/>
-                    <input type="text" placeholder="and take my money" className="meme-input-box"/>
+                    <input type="text" placeholder="Top text" className="meme-input-box" name="topText" onChange={handleChange}/>
+                    <input type="text" placeholder="Bottom text" className="meme-input-box" name= "bottomText" onChange={handleChange}/>
                 </div>
                 <button onClick={getMemes} className="meme-button"><h2>Get a new meme image 🖼️</h2></button>
                 
             </div>
-            <img className="meme-img" src={meme.randomImage} alt="" />
+            <div className="meme">
+                <img className="meme-img" src={meme.randomImage} alt="" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
